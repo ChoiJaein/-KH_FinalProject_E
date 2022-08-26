86eddf3404155962b32636f43b35add9ba95279f
USER <USER@14.47.234.59>
2022-08-26 19:24:51 +0900

    DB 쿼리 전체테이블 초안(2022.08.26)

--- final01/sql/account/account.sql
+++ final01/sql/account/account.sql
@@ -1,3 +1,21 @@
 /**
- *  계정 스키마
+ *  ACCOUNT 스키마
  */
+
+
+ CREATE TABLE ACCOUNT (
+              ACCOUNTID VARCHAR2(50) PRIMARY KEY
+             ,PASSWORD  VARCHAR2(50) NOT NULL
+             ,NAME      VARCHAR2(50) NOT NULL
+             ,EMAIL     VARCHAR2(50) NOT NULL
+             ,ADDRESS   VARCHAR2(100)
+             ,PHONE     VARCHAR2(15)
+             ,BIRTH     DATE
+             ,ADMIN     VARCHAR2(1) CHECK(ADMIN IN('Y', 'N'))
+             ,FILENAME  VARCHAR2(200)
+             ,LOCATION  VARCHAR2(500)
+             ,URL       VARCHAR2(500)  
+ );
+ 
+SELECT * FROM ACCOUNT;
+DROP TABLE ACCOUNT;
