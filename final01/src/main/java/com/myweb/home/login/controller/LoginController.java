15009c5cad48bb93ef2ead05b33a8b590ab45adb
박희망 <hope30529@gmail.com>
2022-08-29 19:28:15 +0900

    hhh

--- final01/src/main/java/com/myweb/home/login/controller/LoginController.java
+++ final01/src/main/java/com/myweb/home/login/controller/LoginController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.myweb.home.login.vo.LoginVO;
+import com.myweb.home.login.model.AccountDTO;
 import com.myweb.home.login.service.LoginService;
 
 
@@ -77,12 +78,59 @@ public class LoginController {
 		return "redirect:/";
 	}
 	
-
+    //아이디,비밀번호 찾기 폼으로 이동
 	@RequestMapping(value = "/login/findIdPwForm", method =RequestMethod.GET)
-	public String findIdPwForm() {
+	public String findIdPwForm() throws Exception {
 		return "/login/findIdPwForm";
 	}
 
-	
-	
-}
+	/*
+	//아이디찾기
+	@RequestMapping(value = "/login/findIdPwForm", method =RequestMethod.POST)
+		public ModelAndView findId(HttpServletRequest request, @RequestParam String mem_email)throws IOException  {
+				
+				ModelAndView mav = new ModelAndView(); 
+				
+				//이메일로 memberDTO값 전체 가져오기. 아이디 찾기는 이메일(unique)를 통해서만 이루어진다. 
+				AccountDTO accountDTO= LoginService.findId(mem_email);
+				
+				if(accountDTO == null) {//해당 이메일이 없을 경우(가입되지 않은 회원일 경우)
+					mav.addObject("findId", null);
+					mav.setViewName("jsonView"); //findId에 null을 담아 페이지로 이동(페이지에서 '등록되지 않은 이메일~' div 경고창 뜬다)
+				}else {
+					//이메일 확인 후, 해당 이메일로 메일 발송
+					String findId= memberDTO.getMem_id();
+					
+					String sender = "brighthannah12@gmail.com";
+					String recipient = mem_email;
+					String title = "▣ 아나바다 마켓 :: 아이디를 확인하세요 ▣";
+					String content = System.getProperty("line.separator")
+									+ System.getProperty("line.separator")
+				                    + "안녕하세요. 아나바다 마켓 아이디 찾기 서비스 확인 메일입니다."
+				                    + System.getProperty("line.separator")
+				                    + System.getProperty("line.separator")
+				                    + "고객님의 아이디는 '" +findId+ "'입니다. "
+				                    + System.getProperty("line.separator")
+				                    + System.getProperty("line.separator")
+				                    + "이용해주셔서 감사합니다."; 
+					try {
+			            MimeMessage message = mailSender.createMimeMessage();
+			            MimeMessageHelper messageHelper = new MimeMessageHelper(message,true, "UTF-8");
+		//	            																↑멀티파트 형식으로 보낸다
+		
+			            messageHelper.setFrom(sender); // 보내는사람. 생략하면 정상작동을 안함
+			            messageHelper.setTo(recipient); // 받는사람 이메일
+			            messageHelper.setSubject(title); // 메일제목. (생략가능)
+			            messageHelper.setText(content); // 메일 내용
+			            
+			            mailSender.send(message);
+			        } catch (Exception e) {
+			            System.out.println(e);
+			        }
+					
+			        mav.addObject("findId", findId);
+			        mav.setViewName("jsonView");
+				}
+		        return mav;
+		        */
+		}
---
+++ final01/src/main/webapp/resources/js/member/findId.js
@@ -0,0 +1,146 @@
+/**
+ * 
+ */
+let pwd_rule=/^.*(?=^.{8,15}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[.!@#$%^&+=]).*$/; 
+let email_rule=/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+
+
+$(document).ready(function(){
+	
+	//처음 페이지 입장하여 버튼 클릭 전까지는 '아이디or비번찾기 폼' 숨기기
+	$('#findIdFormDiv').hide();
+	$('#findPwdFormDiv').hide();
+	
+	//아이디 찾기 버튼을 눌렀을 때
+	$('#chooseIdForm').click(function(){
+		$('#findPwdFormDiv').hide(); //id 찾기 폼만 보이고
+		$('#findIdFormDiv').show();  //pwd 찾기 폼은 숨긴다
+		
+		//입력창 초기화
+		$('#findNameDiv').empty();
+		$('#findEmailDiv').empty();
+		
+		//'아이디 찾기' 버튼 클릭했을 떄,
+		$('#findIdBtn').click(function(){
+			//이메일 입력 유효성 검사(이메일(unique)값으로 확인한다)
+			if($('#email1').val()==''||$('#email2').val()=='')
+				$('#emailDiv').text('이메일을 입력하세요');
+			else{
+				$('#email').val($('#email1').val() + '@' + $('#email2').val()); 
+				$.ajax({
+					type: 'post',
+					url: '/market/member/findId',
+					data: 'mem_email='+$('#email').val(),
+					dataType : 'json',
+					success: function(result){
+						if (JSON.stringify(result.findId)=="null"){//일치하는 이메일 없음
+							$('#emailDiv').text('등록되지 않은 이메일입니다.');
+						}else{//일치하는 데이터있음
+							$('#emailDiv').text('작성하신 이메일로 아이디를 발송하였습니다');
+						}
+					},error: function(err){
+						console.log(err);
+					}
+				});
+			}
+		})
+	});//chooseIdForm
+
+//=============================================================================================
+	//비밀번호 찾기 버튼을 눌렀을때(이메일 인증 -> 비밀번호 수정)
+	//아이디 찾기의 변수와 구분하기 위해 변수명 끝에 'P'를 붙임
+	$('#choosePwdForm').click(function(){
+		$('#findIdFormDiv').hide();
+		$('#findPwdFormDiv').show();
+		
+		//입력창 초기화
+		$('#idDivP').empty();
+		$('#emailDivP').empty();
+		$('#certifyNumDiv').empty();
+		$('#modify_pwd').empty();
+		
+		//인증번호 발송 버튼 클릭
+		$('#sendNumBtnP').click(function(){
+			//입력 유효성 검사
+			if($('#idP').val()=='')
+				$('#idDivP').text('아이디를 입력하세요');
+			else if($('#email1P').val()==''||$('#email2P').val()=='')
+				$('#emailDivP').text('이메일을 입력하세요');
+			
+			//본인 인증 확인 + 인증번호 메일 발송
+			else{
+				$('#emailP').val($('#email1P').val() + '@' + $('#email2P').val()); 
+				
+				$.ajax({
+					type: 'post',
+					url: '/market/member/findPwd',//메일로 인증번호 발송
+					data: 'mem_email='+$('#emailP').val()+'&mem_id='+$('#idP').val(),
+					dataType : 'json',
+					success: function(result){
+						if (JSON.stringify(result.member)=="null"){//일치하는 이메일 없음
+							$('#emailDivP').text('등록되지 않은 이메일입니다.');
+						}else{//일치하는 데이터있음
+							$('#emailDivP').text('작성하신 이메일로 인증번호를 발송하였습니다').css('color','#0a58ca');
+							$('#randomNum').val(result.randomNum);
+							
+							//인증번호 확인 버튼 클릭 시, 인증번호 일치 여부 체크
+							$("#certifyNumBtn").click(function(){
+								let certifyNum = $(this).val();
+								$.ajax({
+									type: 'post',
+									url: '/market/member/confirmPwdcode',
+									data: 'randomNum='+$('#randomNum').val()+"&certifyNum="+$('#certifyNum').val(),
+									dataType: 'json',
+									success: function(result){
+										//인증번호 불일치 시
+										if(result.certifyNum != result.randomNum){
+											$('#certifyNumDiv').text('인증번호가 일치하지 않습니다!');
+										}else {//인증번호 일치 시
+											$('#certifyNumDiv').text('인증 완료되었습니다. 비밀번호를 재설정하세요.').css('color','#0a58ca');
+											
+											//비밀번호 재설정	
+											$('#resetPwdBtn').click(function(){
+												//입력 유효성 검사
+												if($('#resetPwd').val()==''){
+													$('#resetPwdDiv').text('재설정할 비밀번호를 입력하세요');
+													if(!pwd_rule.test($('#resetPwd').val())) //8-15글자. 특문+영문+숫자 조합
+														$('#resetPwdDiv').text('올바르지 않은 비밀번호 형식입니다.');
+												}else if($('#certifyPwd').val()==''){
+													$('certifyPwdDiv').text('비밀번호를 다시 입력하세요');
+													if($('#resetPwd').val() != $('#certifyPwd').val())
+														$('#certifyPwdDiv').text('비밀번호가 일치하지 않습니다');
+												}else{
+													if($('#resetPwd').val() != $('#certifyPwd').val())
+														$('#certifyPwdDiv').text('비밀번호가 일치하지 않습니다');
+													
+													//새 비밀번호 변경
+													else{
+														$('#certifyPwdDiv').text('비밀번호가 일치합니다');
+															$.ajax({
+																type: 'post',
+																url: '/market/member/resetPwd',
+																data: 'mem_email='+$('#emailP').val()+'&mem_pwd='+$('#resetPwd').val(),
+																dataType : 'text',
+																success: function(result){
+																	$('#certifyPwdDiv').text('비밀번호가 성공적으로 설정되었습니다.').css('color','#0a58ca');
+																},error: function(err){
+																	console.log(err);
+																}
+															});//ajax
+														}
+													}
+												});//#resetPwdBtn
+										}
+									}
+								});
+							});
+							
+						}
+					},error: function(err){
+						console.log(err);
+					}
+				});
+			}	//else
+		});//sendNumBtnP
+	});//choosePwdForm
+});	//document.ready
